4ff5f7b53aa784ebc5b2c3c7482212a9e1e6cf5b
Назар Зиевич Беко <dlvthelove@gmail.com>
2025-03-25 00:37:29 +0800

    сделал график и обносил вид SugarNoteView

---
+++ DiaHelp/ViewModel/SugarChartDrawable.cs
@@ -0,0 +1,64 @@
+﻿using DiaHelp.Model;
+using Microsoft.Maui.Graphics;
+using System.Collections.Generic;
+
+namespace DiaHelp.ViewModel
+{
+    public class SugarChartDrawable : IDrawable
+    {
+        private readonly IEnumerable<SugarModel> _sugarNotes;
+
+        public SugarChartDrawable(IEnumerable<SugarModel> sugarNotes)
+        {
+            _sugarNotes = sugarNotes;
+        }
+
+        public void Draw(ICanvas canvas, RectF dirtyRect)
+        {
+            if (_sugarNotes == null || !_sugarNotes.Any()) return;
+
+            // Настройка размеров графика
+            var margin = 40;
+            var graphWidth = dirtyRect.Width - 2 * margin;
+            var graphHeight = dirtyRect.Height - 2 * margin;
+
+            // Очистка фона
+            canvas.FillColor = Colors.Transparent;
+            canvas.FillRectangle(dirtyRect);
+
+            // Отрисовка осей
+            // Горизонтальная ось
+
+            // Отрисовка линии графика
+            canvas.StrokeColor = Colors.Purple;
+            canvas.StrokeSize = 3;
+
+            var maxSugarLevel = (float)_sugarNotes.Max(n => n.SugarLevel); // Максимальное значение уровня сахара
+            var points = new List<PointF>();
+
+            foreach (var (note, index) in _sugarNotes.Select((n, i) => (n, i)))
+            {
+                var x = margin + (index / (float)(_sugarNotes.Count() - 1)) * graphWidth;
+                var y = dirtyRect.Height - margin - ((float)note.SugarLevel / maxSugarLevel) * graphHeight; // Преобразование SugarLevel в float
+                points.Add(new PointF(x, y));
+            }
+
+            for (int i = 1; i < points.Count; i++)
+            {
+                canvas.DrawLine(points[i - 1].X, points[i - 1].Y, points[i].X, points[i].Y);
+            }
+
+            // Отрисовка меток на осях
+            canvas.FontSize = 20;
+            canvas.FontColor = Colors.White;
+
+            foreach (var (note, index) in _sugarNotes.Select((n, i) => (n, i)))
+            {
+                var x = margin + (index / (float)(_sugarNotes.Count() - 1)) * graphWidth;
+                var y = dirtyRect.Height - margin - ((float)note.SugarLevel / maxSugarLevel) * graphHeight;
+
+                canvas.DrawString(note.SugarLevel.ToString(), x - 10, y - 20, HorizontalAlignment.Center);
+            }
+        }
+    }
+}
--- DiaHelp/ViewModel/SugarNoteViewModel.cs
+++ DiaHelp/ViewModel/SugarNoteViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using DiaHelp.Interface;
 using DiaHelp.Model;
+using Microsoft.Maui.Graphics;
 using System.Collections.ObjectModel;
 using System.Windows.Input;
+using static Microsoft.Diagnostics.Tracing.Analysis.GC.TraceGC;
 
 namespace DiaHelp.ViewModel
 {
@@ -12,7 +14,9 @@ namespace DiaHelp.ViewModel
         private decimal _sugarLevel { get; set; }
         public string MeasurementTime { get; set; }
         public string _mealType { get; set; }
+
         public ObservableCollection<SugarModel> SugarNotes { get; set; }
+        
 
         public SugarNoteViewModel(IDatabaseService databaseService, IWindowService windowService)
         {
@@ -24,6 +28,7 @@ namespace DiaHelp.ViewModel
             Clear = new RelayCommand(ClearNote);
             SugarAverage = new RelayCommand(Average);
             LoadSugarNotes();
+
         }
 
         public decimal SugarLevel
@@ -46,16 +51,21 @@ namespace DiaHelp.ViewModel
             }
         }
 
-        public async void ClearNote(object parametr)
+        private void UpdateChart()
         {
-            if (_databaseService.ClearAllSugarNotes())
+            ChartDrawable = new SugarChartDrawable(SugarNotes);
+            OnPropertyChanged(nameof(ChartDrawable));
+        }
+
+        private void LoadSugarNotes()
+        {
+            var notes = _databaseService.GetAllSugarNotes();
+            SugarNotes.Clear();
+            foreach (var note in notes)
             {
-                SugarNotes.Clear();
+                SugarNotes.Add(note);
             }
-            else
-            {
-                await Application.Current.MainPage.DisplayAlert("Ошибка", "Не удалось очистить записи", "OK");
-            }
+            UpdateChart(); // Обновляем график при загрузке данных
         }
 
         public async void AddSugarNote(object parameter)
@@ -82,29 +92,30 @@ namespace DiaHelp.ViewModel
                 SugarNotes.Insert(0, sugarNote);
                 SugarLevel = 0;
                 MealType = string.Empty;
+                UpdateChart();
             }
+            
         }
 
-        public void Average(object parametr)
-        {
-            Application.Current.MainPage = _windowService.GetAndCreateContentPage<SugarAverageLevelViewModel>().View;
-        }
+        private void MainGo(object parameter) => Application.Current.MainPage = _windowService.GetAndCreateContentPage<MainViewModel>().View;
 
-        private void LoadSugarNotes()
+        private void ClearNote(object parameter)
         {
-            var notes = _databaseService.GetAllSugarNotes();
-            SugarNotes.Clear();
-            foreach (var note in notes)
+            if (_databaseService.ClearAllSugarNotes())
             {
-                SugarNotes.Add(note);
+                SugarNotes.Clear();
+                UpdateChart(); // Обновляем график после очистки записей
             }
         }
 
-        private void MainGo(object parametr) => Application.Current.MainPage = _windowService.GetAndCreateContentPage<MainViewModel>().View;
+        private void Average(object parameter) => Application.Current.MainPage = _windowService.GetAndCreateContentPage<SugarAverageLevelViewModel>().View;
+
+        public IDrawable ChartDrawable { get; set; }
 
         public ICommand Clear { get; }
         public ICommand MainPage { get; }
         public ICommand AddSugarNoteCommand { get; }
         public ICommand SugarAverage { get; }
+        public ICommand EntryData { get; }
     }
 }
