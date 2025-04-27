d87f42baa548e28521c2f065778585aa545aa2bb
Назар Зиевич Беко <dlvthelove@gmail.com>
2025-04-27 18:16:41 +0800

    чистка кода

--- DiaHelp/Interface/IAiChatService.cs
+++ DiaHelp/Interface/IAiChatService.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DiaHelp.Interface
+﻿namespace DiaHelp.Interface
 {
     public interface IAiChatService
     {
